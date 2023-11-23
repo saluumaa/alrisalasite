52de6a7515de7d7876b3483d7fa1db9b19ee600d
saluumaa <salumahassan02@gmail.com>
2023-11-23 10:36:04 +0300

    Create react app

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <h2> Al-Risala Organization is a non-profit organization that focuses on education and social activities.
+      it was established in 2017 and has been active in various fields such as education, social, and religion.
+      </h2>
+
+      <h2>Our Vision</h2>
+      <h3>Al-Risala Organization is a non-profit organization that focuses on education and social activities.
+      it was established in 2017 and has been active in various fields such as education, social, and religion.
+      </h3>
+
+      <h2>Our Mission</h2>
+      <h3>Al-Risala Organization is a non-profit organization that focuses on education and social activities.
+      it was established in 2017 and has been active in various fields such as education, social, and religion.
+      </h3>
+
+      <h2>Our Values</h2>
+      <h3>Al-Risala Organization is a non-profit organization that focuses on education and social activities.
+      it was established in 2017 and has been active in various fields such as education, social, and religion.
+      </h3>
+
+      <h2>Our Goals</h2>
+      <h3>Al-Risala Organization is a non-profit organization that focuses on education and social activities.
+      it was established in 2017 and has been active in various fields such as education, social, and religion.
+      </h3>
+
+      <h2>Our Objectives</h2>
+
+      <h3>Al-Risala Organization is a non-profit organization that focuses on education and social activities.
+      it was established in 2017 and has been active in various fields such as education, social, and religion.
+      </h3>
+      
+      
+    </div>
+  )
+}
+
+export default About
