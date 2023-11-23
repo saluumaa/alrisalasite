15d0eb76c1340f6a7a2e3761a16a8cab13ac4db5
saluumaa <salumahassan02@gmail.com>
2023-11-23 10:34:05 +0300

    Create home page

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../Hero/Hero'
+import Service from '../Services/Service'
+// import Donate from '../Donate/Donate'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      {/* <Donate /> */}
+      <Service />
+    </div>
+  )
+}
+
+export default Home
