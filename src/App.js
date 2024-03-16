72cd6c9ffb8c6d36b0cbf7b92f9ca8cddbd63a23
saluumaa <salumahassan02@gmail.com>
2024-03-16 20:57:59 +0300

    Create react alrisala app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React  from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
@@ -10,24 +11,29 @@ import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
 import Login from './Components/Login/Login';
 import AddNewNews from './Components/News/AddNewNews';
+import Volunteer from './Components/Volunteer/Volunteer';
+import store from './redux/store';
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  // const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div className="App">
+      <Provider store={store}>
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/news/:id" element={<FullNews />} />
-          <Route path="/news" element={<News loggedIn={loggedIn} />} />
+          <Route path="/news" element={<News />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/addnews" element={<AddNewNews />} />
-          <Route path="/login" element={<Login loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+          <Route path="/volunteer" element={<Volunteer />} />
+          <Route path="/login" element={<Login  />} />
         </Routes>
         <Footer />
       </BrowserRouter>
+      </Provider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import { GlobalProvider } from './Components/Context/GlobalState';
+// import { GlobalProvider } from './Components/Context/GlobalState';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalProvider>
       <App />
-    </GlobalProvider>
   </React.StrictMode>
 );
 
