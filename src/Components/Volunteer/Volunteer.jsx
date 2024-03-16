0a72aa0b36827a69818f64ec7faa00beba24e6e3
saluumaa <salumahassan02@gmail.com>
2024-03-16 20:53:52 +0300

    Add volunteer component

---
+++ src/Components/Volunteer/Volunteer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Volunteer.css'
+const Volunteer = () => {
+  return (
+    <div className='volunteer-container'>
+        <p> We are sorry but the volunteer form is currently unavailable. Please check back later.
+        </p></div>
+  )
+}
+
+export default Volunteer
