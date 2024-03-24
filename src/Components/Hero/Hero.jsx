5c30e71f1dc667b4eada357b9fcb1fa2d4e03514
saluumaa <salumahassan02@gmail.com>
2024-03-24 07:13:19 +0300

    Add changes to the home components

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,48 +9,56 @@ import Donate from '../Donate/Donate'
 
 const Hero = () => {
 const images = [ hero_image, hero_image2, hero_image3 ]
+const imageText = ['Empowering Youth Through Education and building better future together.', 'We are committed to providing a platform for the youth to develop their skills and talents.', 'We are dedicated to providing a platform for the youth to develop their skills and talents.']
 const [index, setIndex] = useState(0)
 
 useEffect(() => {
   const interval = setInterval(() => {
     setIndex(index => (index + 1) % images.length)
-  }, 3000)
+  }, 3000);
   return () => clearInterval(interval)
 } , [ images.length])
 
 
   return (
-    <div className='hero-wrapper'>
-    <div className='hero'>
+    <section className='hero-wrapper'>
+    {/* <div className='hero'> */}
 			<div className='image-wrapper'>
 				<img src={
           images[index]
         } alt={`Organization Pictures  ${index + 1 }`} />
         <div className='image-overlay'>
           <p>
-            Empowering Youth Through Education and building better future together.
+            {imageText[index]}  
           </p>
+        <section className='social-icons'>
+          <div>
           <Link to="/about"><button className='btn' >Learn More</button></Link>
+          </div>
+          <div>
+          <button>Follow Us  
+          <a href='https://www.facebook.com/AlrisalaOrganization/' target='_blank' rel="noopener noreferrer" style={{paddingLeft: '10px'}} ><FaFacebook /></a>
+          <a href='https://www.tiktok.com/@alrisalaorg' target='_blank' rel="noopener noreferrer"><FaTiktok /></a>
+          <a href='https://www.youtube.com/channel/UCQ8aXs3FQxXp3zY6t9XqC6g' target='_blank' rel="noopener noreferrer"><FaYoutube /></a>
+          </button>
+          </div>
+      </section>
         </div>
-			</div>
+        </div>
+      <Donate className="donate" />
+     </section>
+  )
+}
+        
+			{/* </div> */}
      
-
-			<div className='hero-content'>
+			{/* <div className='hero-content'>
         <h1 style={{color: '#489EDE', fontFamily: "'lobster', sans-serif"}}>WELCOME TO</h1>
         <h2>AL-RISALA ORGANIZATION</h2>
         <p> Is a non-profit organization that aims to provide a platform for the youth to develop their skills and talents in order to become the future leaders of the community.
         </p>
-        <button className='btn'>Follow Us </button>
-        <div className='social-icons'>
-          <a href='https://www.facebook.com/AlrisalaOrganization/' target='_blank' rel="noopener noreferrer"><FaFacebook /></a>
-          <a href='https://www.tiktok.com/@alrisalaorg' target='_blank' rel="noopener noreferrer"><FaTiktok /></a>
-          <a href='https://www.youtube.com/channel/UCQ8aXs3FQxXp3zY6t9XqC6g' target='_blank' rel="noopener noreferrer"><FaYoutube /></a>
-      </div>
-    </div>
-    </div>
-    <Donate />
-     </div>
-  )
-}
+        
+    </div> */}
+    
 
 export default Hero
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,52 +1,63 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
-import { FaBars, FaTimes } from 'react-icons/fa'
-import logo from '../Assets/logo_background.jpeg'
-import DarkMode from '../DarkMode/DarkMode'
-import './Navbar.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaBars, FaTimesCircle } from 'react-icons/fa';
+import logo from '../Assets/logo_background.jpeg';
+import DarkMode from '../DarkMode/DarkMode';
+import './Navbar.css';
 
 const Navbar = () => {
-  const [menu, setMenu] = useState('home')
-  const [isActive, setIsActive ] = useState(false)
+  const [menu, setMenu] = useState('home');
+  const [isActive, setIsActive] = useState(false);
 
   const handleClick = () => {
-    setIsActive(!isActive)
-  }
+    setIsActive(!isActive);
+  };
+
+  const handleLinkClick = () => {
+    setIsActive(false); 
+  };
+
   return (
-    <>
-    <nav className={isActive ? 'nav-active' : ''}>
-        <div className='navbar-logo'>
+    <aside>
+      <nav className={isActive ? 'nav-active' : ''}>
+        <section className="top-nav-wrapper">
+          <div className='navbar-logo'>
             <img src={logo} alt='logo' />
-        </div>
-        <ul className='navbar-links'>
-					<li onClick={()=> {setMenu('home')}}>
-					<Link style={{textDecoration: 'none'}} to="/">Home</Link> {menu === 'home' ? <hr /> : <></>}
-					</li>
-					<li onClick={()=> {setMenu('about')}}>
-					<Link style={{textDecoration: 'none'}} to="/about">About Us</Link> {menu === 'about' ? <hr /> : <></>}
-					</li>
-					<li onClick={()=> {setMenu('news')}}>
-					<Link style={{textDecoration: 'none'}} to="/news">News</Link> {menu === 'news' ? <hr /> : <></>}
-					</li>
-					<li onClick={()=> {setMenu('contact')}}>
-					<Link style={{textDecoration: 'none'}} to="/contact">Contact Us</Link> {menu === 'contact' ? <hr /> : <></>}
-					</li>
-        </ul>
+          </div>
+          <div className='login-dark-mode'>
+            <div className='dark-mode'>
+              <DarkMode />
+            </div>
+            <div className='login-btn'>
+              <Link to="/login"><button onClick={handleLinkClick} >Log in</button></Link>
+            </div>
+          </div>
+        </section>
+        <section className='navbar-wrapper'>
+          <ul className='navbar-links'>
+            <li onClick={()=> {setMenu('home')}}>
+              <Link style={{textDecoration: 'none'}} to="/" onClick={handleLinkClick}>Home</Link> {menu === 'home' ? <hr /> : <></>}
+            </li>
+            <li onClick={()=> {setMenu('about')}}>
+              <Link style={{textDecoration: 'none'}} to="/about" onClick={handleLinkClick}>About Us</Link> {menu === 'about' ? <hr /> : <></>}
+            </li>
+            <li onClick={()=> {setMenu('news')}}>
+              <Link style={{textDecoration: 'none'}} to="/news" onClick={handleLinkClick}>News</Link> {menu === 'news' ? <hr /> : <></>}
+            </li>
+            <li onClick={()=> {setMenu('contact')}}>
+              <Link style={{textDecoration: 'none'}} to="/contact" onClick={handleLinkClick}>Contact Us</Link> {menu === 'contact' ? <hr /> : <></>}
+            </li>
+          </ul>
+        </section>
+      </nav>
+      <div className='hamburger' onClick={handleClick}>
+        {
+          isActive ? <FaTimesCircle /> : <FaBars />
+        }
 
-       
-        <div className='login-btn'>
-        <div className='dark-mode'>
-          <DarkMode />
-        </div>
-        <Link to="/login"><button>Log in</button></Link>    
-        </div>
-    </nav>
-    
-  <div className='humberger' onClick={handleClick}>
-  {isActive ? <FaTimes /> : <FaBars />}
-  </div>
-  </>
-  )
-}
+      </div>
+    </aside>
+  );
+};
 
-export default Navbar
+export default Navbar;
